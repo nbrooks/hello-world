7a8b206a1dc73ab9d62aef608413177482625eed
Nick Brooks <nicholas.p.brooks@gmail.com>
2016-10-20 10:23:58 -0500

    Tomcat support

--- src/main/java/io/nickbrooks/tech/DemoApplication.java
+++ src/main/java/io/nickbrooks/tech/DemoApplication.java
@@ -1,12 +1,27 @@
 package io.nickbrooks.tech;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+    private static Class<DemoApplication> applicationClass = DemoApplication.class;
 }
---
+++ src/main/java/io/nickbrooks/tech/controllers/TestController.java
@@ -0,0 +1,15 @@
+package io.nickbrooks.tech.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/hello/{name}")
+    String hello(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+
+}
